b896430592041ce42c00fffa5dcbe2c30389c9dc
kumarpoudel27 <mepoudelkumar@gmail.com>
2025-06-01 15:15:35 -0500

    used right url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,7 +33,7 @@ function App() {
     } else {
       formData.append("text", inputText);
     }
-    const res = await fetch(""https://apa-formatter-backend.onrender.com/format-apa/", {
+    const res = await fetch("https://apa-formatter-backend.onrender.com/format-apa/", {
       method: "POST",
       body: formData,
     });
@@ -52,7 +52,7 @@ function App() {
       formData.append("text", inputText);
     }
     formData.append("output_format", "docx");
-    const res = await fetch("http://localhost:8000/format-apa/", {
+    const res = await fetch("https://apa-formatter-backend.onrender.com/format-apa/", {
       method: "POST",
       body: formData,
     });
